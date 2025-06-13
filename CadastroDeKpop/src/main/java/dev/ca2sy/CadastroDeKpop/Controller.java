7d9dd4fded9e4f6ab79aa6a53a08ec76b35070c1
Rafaela Sousa Costa <rafaelaengsousa@gmail.com>
2025-06-13 10:07:59 -0300

    inicializando projeto

--- CadastroDeKpop/src/main/java/dev/ca2sy/CadastroDeKpop/Controller.java
+++ CadastroDeKpop/src/main/java/dev/ca2sy/CadastroDeKpop/Controller.java
@@ -1,8 +1,17 @@
 package dev.ca2sy.CadastroDeKpop;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class Controller {
+
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return ("Primeira mensagem nesse portal");
+    }
     
 }
