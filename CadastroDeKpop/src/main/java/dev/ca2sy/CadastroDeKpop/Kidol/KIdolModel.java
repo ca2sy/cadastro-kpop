4aab7ae750b0ac3dd3e174937746cd141e119b7d
Rafaela Sousa Costa <rafaelaengsousa@gmail.com>
2025-06-16 12:06:36 -0300

    trabalhando com lombok

--- CadastroDeKpop/src/main/java/dev/ca2sy/CadastroDeKpop/Kidol/KIdolModel.java
+++ CadastroDeKpop/src/main/java/dev/ca2sy/CadastroDeKpop/Kidol/KIdolModel.java
@@ -1,13 +1,14 @@
 package dev.ca2sy.CadastroDeKpop.Kidol;
-
-import java.util.List;
-
 import dev.ca2sy.CadastroDeKpop.Show.ShowModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 
 @Entity
 @Table(name = "tb_cadastro")
+@AllArgsConstructor //cria automaticamente um construtor com todos os argumentos
+@Data //cria automaticamente getters e setters
 public class KIdolModel { //classe é um modelo pra um objeto
    
     //parei: preciso transformar classe em entidade, baixar dependencia
@@ -20,40 +21,10 @@ public class KIdolModel { //classe é um modelo pra um objeto
     private String nome;
     private String email;
     private int idade;
+
     @ManyToOne 
     @JoinColumn(name = "shows_id") //chave estrangeira
      private ShowModel show;
 
-    
-    public KIdolModel(String nome, String email, int idade){
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    //preciso transformar isso em uma tabela que vai pro banco de dados! isso que significa criar entidades
 }
--- CadastroDeKpop/src/main/java/dev/ca2sy/CadastroDeKpop/Show/ShowModel.java
+++ CadastroDeKpop/src/main/java/dev/ca2sy/CadastroDeKpop/Show/ShowModel.java
@@ -1,10 +1,14 @@
 package dev.ca2sy.CadastroDeKpop.Show;
 import dev.ca2sy.CadastroDeKpop.Kidol.KIdolModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 
 @Entity
 @Table (name = "tb_shows")
+@AllArgsConstructor //cria automaticamente um construtor com todos os argumentos
+@Data //cria automaticamente getters e setters
 public class ShowModel {
 
     @Id
@@ -15,44 +19,6 @@ public class ShowModel {
 
     @OneToMany (mappedBy = "show") //chave estrangeira: conectei relacionamento
     private KIdolModel cantor;
-
-    public ShowModel(String nome, String pais){
-        this.nome = nome;
-        this.pais = pais;
-    }
-
-    public KIdolModel getCantor() {
-        return cantor;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setCantor(KIdolModel cantor) {
-        this.cantor = cantor;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
 
 
 }
